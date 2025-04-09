cd85175039b032d494b292df7af8c02e5b71b535
Gabriel <gabspereira178@gmail.com>
2025-04-09 14:17:39 +0100

    both now card and selector use the color pass from pokemon data but instead of hardcoed use utils colors class that has the paring for bgc and text

--- src/components/pokemonCard.jsx
+++ src/components/pokemonCard.jsx
@@ -1,16 +1,4 @@
-const colorClasses = {
-    black: 'bg-gray-900',
-    blue: 'bg-blue-500',
-    brown: 'bg-amber-500',
-    gray: 'bg-gray-500',
-    green: 'bg-green-500',
-    pink: 'bg-pink-500',
-    purple: 'bg-purple-500',
-    red: 'bg-red-400',
-    white: 'bg-stone-200',
-    yellow: 'bg-yellow-500',
-};
-
+import { colorClasses } from "../utils/colors";
 export const PokemonCard = ({
     name,
     type,
@@ -29,32 +17,34 @@ export const PokemonCard = ({
     showExtraInfo = false,
 }) => {
     // Get background color based on type
-    const backgroundColor = colorClasses[color.toLowerCase()];
+    const colors = colorClasses[color.toLowerCase()];
 
     return (
         <div>
+            {/*CARD CONTAINER*/}
             <div
                 className={`relative w-[500px] ${
                     showExtraInfo ? 'h-[570px]' : 'h-[470px]'
-                } flex flex-col rounded-2xl transition-all duration-300 ease-in-out overflow-hidden ${backgroundColor}`}
+                } flex flex-col rounded-2xl transition-all duration-300 ease-in-out overflow-hidden ${colors.background}`}
             >
+                {/*CARD OPACITY CONTAINER */}
                 {
                     <div className="absolute w-full h-full pointer-events-none opacity-10 rounded-2xl"></div>
                 }
-
+                {/*IMAGE CONAINER */}
                 <div className="relative flex flex-col items-center justify-center mb-4">
                     <img
                         className="w-[200px] h-[200px]"
                         src={imageUrl || `/api/placeholder/200/200`}
                         alt={`${name} Pokemon`}
                     />
+                    {/* BADGE TYPE CONTAINER */}
                     <div className="bg-stone-50 text-2xl flex justify-center items-center w-[200px] h-[40px] rounded-lg text-gray-800 font-medium">
                         <span>{type}</span>
                     </div>
                 </div>
-
                 <div className="flex flex-row justify-between p-[16px] mb-4">
-                    <h2 className="pl-[16px] text-teal-50 font-bold">{name}</h2>
+                    <h2 className={`pl-[16px] ${colors.text} font-bold1`}>{name}</h2>
                     <button className="pr-[16px] appearance-none border-0 bg-transparent cursor-pointer">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -62,7 +52,7 @@ export const PokemonCard = ({
                             viewBox="0 0 24 24"
                             strokeWidth="1.5"
                             stroke="currentColor"
-                            className="w-[32px] h-[32px] text-white"
+                            className={`w-[32px] h-[32px] ${colors.text}`}
                         >
                             <path
                                 strokeLinecap="round"
@@ -73,13 +63,12 @@ export const PokemonCard = ({
                     </button>
                 </div>
                 {/* VISIBLE INFO CONTAINER */}
-                <div className="grid grid-cols-2 grid-rows-3 mb-6 gap-2 px-[16px] text-stone-50">
+                <div className={`grid grid-cols-2 grid-rows-3 mb-6 gap-2 px-[16px] ${colors.text}`}>
                     {/* Health container */}
                     <div className="flex items-center justify-between px-4 py-0">
                         <span className="text-base font-normal">Health</span>
                         <span className="text-base font-bold">{health}</span>
                     </div>
-
                     <div className="flex items-center justify-between px-4 py-0">
                         <span className="text-base font-normal">Main Ability</span>
                         <span className="text-base font-bold">{mainAbility}</span>
@@ -105,9 +94,8 @@ export const PokemonCard = ({
                         <span className="text-base font-bold">{friendliness}</span>
                     </div>
                 </div>
-
                 {showExtraInfo && (
-                    <div className="grid grid-cols-2 grid-rows-2 border-t border-teal-50 gap-2 px-[16px] h-[100px] text-teal-50">
+                    <div className={`grid grid-cols-2 grid-rows-2 border-t border-teal-50 gap-2 px-[16px] h-[100px] ${colors.text}`}>
                         <div className="flex justify-between items-center py-0 px-[16px]">
                             <span className="text-base font-normal">Height</span>
                             <span className="text-base font-bold">{height}</span>
--- src/components/pokemonSelector.jsx
+++ src/components/pokemonSelector.jsx
@@ -1,21 +1,10 @@
 import { useState } from 'react';
-
-const colorClasses = {
-    black: 'bg-gray-900',
-    blue: 'bg-blue-500',
-    brown: 'bg-amber-500',
-    gray: 'bg-gray-500',
-    green: 'bg-green-500',
-    pink: 'bg-pink-500',
-    purple: 'bg-purple-500',
-    red: 'bg-red-400',
-    white: 'bg-stone-200',
-    yellow: 'bg-yellow-500',
-};
+import { PokemonOption } from './pokemonOption';
+import { colorClasses } from '../utils/colors';
 
 export const PokemonSelector = ({ onSelectedPokemon, currentPokemon, color }) => {
     const [isOpen, setIsDropdownOpen] = useState(false);
-    const backgroundColor = colorClasses[color.toLowerCase()];
+    const colors = colorClasses[color.toLowerCase()];
 
     const toggleDropdown = () => {
         setIsDropdownOpen(!isOpen);
@@ -39,15 +28,16 @@ export const PokemonSelector = ({ onSelectedPokemon, currentPokemon, color }) =>
         'Alakazam',
         'Charizard',
         'Flareon',
+        'abc',
     ];
 
     return (
         <div>
             <div
-                className={`${backgroundColor} relative rounded-2xl w-[500px] h-[134px] flex flex-col items-center justify-center mb-4`}
+                className={`${colors.background} relative rounded-2xl w-[500px] h-[134px] flex flex-col items-center justify-center mb-4`}
             >
                 <div className="absolute w-full h-full opacity-10"></div>
-                <h1 className="mb-4 text-4xl text-teal-50">Pokémon</h1>
+                <h1 className="mb-4 text-4xl text-stone-50">Pokémon</h1>
                 <div className="relative flex justify-center w-[80%] rounded-2xl">
                     <div className="absolute w-full h-full pointer-events-none bg-stone-50 opacity-20 rounded-2xl"></div>
                     <button
@@ -58,7 +48,7 @@ export const PokemonSelector = ({ onSelectedPokemon, currentPokemon, color }) =>
                     </button>
                     {isOpen && (
                         <div
-                            className={` ${backgroundColor} rounded-2xl border-t border-t-stone-50 top-[120%] absolute h-auto w-full  flex-col items-center justify-center z-50`}
+                            className={` ${colors.background} rounded-2xl border-t border-t-stone-50 top-[120%] absolute h-auto w-full  flex-col items-center justify-center z-50`}
                         >
                             <div className="absolute w-full h-full pointer-events-none opacity-20 bg-stone-50 rounded-2xl"></div>
                             {pokemonList.map(pokemon => (
@@ -76,14 +66,3 @@ export const PokemonSelector = ({ onSelectedPokemon, currentPokemon, color }) =>
     );
 };
 
-const PokemonOption = ({ value, onClick }) => {
-    return (
-        <button
-            className="w-full cursor-pointer h-[30px] appearance-none border-none mt-2 bg-transparent text-[20px] text-stone-50"
-            value={value}
-            onClick={() => onClick(value)}
-        >
-            {value}
-        </button>
-    );
-};
