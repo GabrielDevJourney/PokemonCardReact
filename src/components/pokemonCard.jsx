f852075d75b36ce513224cbf7fd2200cd2871041
Gabriel <gabspereira178@gmail.com>
2025-04-12 10:55:27 +0100

    made button to show more info into a component and also the pokemon info divs also become a simple component both have been implemented with sucess

--- src/components/pokemonCard.jsx
+++ src/components/pokemonCard.jsx
@@ -1,5 +1,9 @@
-import { COLORS } from "../constants/pokemons";
-export const PokemonCard = ({pokemonData}) => {
+import { COLORS } from '../constants/pokemons';
+import { ExtraInfoButton } from './extraInfoButton';
+import { useState } from 'react';
+import { PokemonInfo } from './pokemonInfo';
+
+export const PokemonCard = ({ pokemonData }) => {
     const {
         name,
         type,
@@ -15,19 +19,18 @@ export const PokemonCard = ({pokemonData}) => {
         weight,
         hiddenAbility,
         speed,
-        showExtraInfo,
     } = pokemonData;
 
     // Get background color based on type
     const colors = COLORS[color.toLowerCase()];
-
+    const [showExtraInfo, setShowExtraInfo] = useState(false);
     return (
         <div>
             {/*CARD CONTAINER*/}
             <div
                 className={`relative w-[500px] ${
-                    showExtraInfo ? 'h-[570px]' : 'h-[470px]'
-                } flex flex-col rounded-2xl transition-all duration-300 ease-in-out overflow-hidden ${
+                    showExtraInfo ? 'h-[550px]' : 'h-[470px]'
+                } flex flex-col rounded-2xl transition-all duration-500 ease-in-out overflow-hidden ${
                     colors.background
                 }`}
             >
@@ -48,76 +51,33 @@ export const PokemonCard = ({pokemonData}) => {
                     </div>
                 </div>
                 <div className="flex flex-row justify-between p-[16px] mb-4">
-                    <h2 className={`pl-[16px] ${colors.text} font-bold1`}>{name}</h2>
-                    <button className="pr-[16px] appearance-none border-0 bg-transparent cursor-pointer">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth="1.5"
-                            stroke="currentColor"
-                            className={`w-[32px] h-[32px] ${colors.text}`}
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
-                            />
-                        </svg>
-                    </button>
+                    <h2 className={`pl-[16px] text-3xl ${colors.text} font-bold1`}>{name}</h2>
+                    {/* BUTTON EXTEND INFO */}
+                    <ExtraInfoButton
+                        colors={colors}
+                        showExtraInfo={showExtraInfo}
+                        setShowExtraInfo={setShowExtraInfo}
+                    ></ExtraInfoButton>
                 </div>
                 {/* VISIBLE INFO CONTAINER */}
-                <div className={`grid grid-cols-2 grid-rows-3 mb-6 gap-2 px-[16px] ${colors.text}`}>
+                <div className={`grid grid-cols-2 grid-rows-3 mb-6 gap-2 px-4 ${colors.text}`}>
                     {/* Health container */}
-                    <div className="flex items-center justify-between px-4 py-0">
-                        <span className="text-base font-normal">Health</span>
-                        <span className="text-base font-bold">{health}</span>
-                    </div>
-                    <div className="flex items-center justify-between px-4 py-0">
-                        <span className="text-base font-normal">Main Ability</span>
-                        <span className="text-base font-bold">{mainAbility}</span>
-                    </div>
-
-                    <div className="flex items-center justify-between px-4 py-0">
-                        <span className="text-base font-normal">Habitat</span>
-                        <span className="text-base font-bold">{habitat}</span>
-                    </div>
-
-                    <div className="flex items-center justify-between px-4 py-0">
-                        <span className="text-base font-normal">Power</span>
-                        <span className="text-base font-bold">{power}</span>
-                    </div>
-
-                    <div className="flex items-center justify-between px-4 py-0">
-                        <span className="text-base font-normal">Capture Rate</span>
-                        <span className="text-base font-bold">{captureRate}</span>
-                    </div>
-
-                    <div className="flex items-center justify-between px-4 py-0">
-                        <span className="text-base font-normal">Friendliness</span>
-                        <span className="text-base font-bold">{friendliness}</span>
-                    </div>
+                    <PokemonInfo typeOfData={'Health'} data={health}></PokemonInfo>
+                    <PokemonInfo typeOfData={'Main Ability'} data={mainAbility}></PokemonInfo>
+                    <PokemonInfo typeOfData={'Habitat'} data={habitat}></PokemonInfo>
+                    <PokemonInfo typeOfData={'Power'} data={power}></PokemonInfo>
+                    <PokemonInfo typeOfData={'Capture Rate'} data={captureRate}></PokemonInfo>
+                    <PokemonInfo typeOfData={'Friendliness'} data={friendliness}></PokemonInfo>
                 </div>
+                {/* HIDDEN INFO CONTAINER */}
                 {showExtraInfo && (
                     <div
-                        className={`grid grid-cols-2 grid-rows-2 border-t border-teal-50 gap-2 px-[16px] h-[100px] ${colors.text}`}
+                        className={`grid grid-cols-2 grid-rows-2 border-t border-teal-50 gap-2 px-4 pt-4 h-20 transition-all duration-300 ease-in-out ${colors.text}`}
                     >
-                        <div className="flex justify-between items-center py-0 px-[16px]">
-                            <span className="text-base font-normal">Height</span>
-                            <span className="text-base font-bold">{height}</span>
-                        </div>
-                        <div className="flex justify-between items-center py-0 px-[16px]">
-                            <span className="text-base font-normal">Weight</span>
-                            <span className="text-base font-bold">{weight}</span>
-                        </div>
-                        <div className="flex justify-between items-center py-0 px-[16px]">
-                            <span className="text-base font-normal">Hidden Ability</span>
-                            <span className="text-base font-bold">{hiddenAbility}</span>
-                        </div>
-                        <div className="flex justify-between items-center py-0 px-[16px]">
-                            <span className="text-base font-normal">Speed</span>
-                            <span className="text-base font-bold">{speed}</span>
-                        </div>
+                        <PokemonInfo typeOfData="Height" data={height}></PokemonInfo>
+                        <PokemonInfo typeOfData="Weight" data={weight}></PokemonInfo>
+                        <PokemonInfo typeOfData="Hidden Ability" data={hiddenAbility}></PokemonInfo>
+                        <PokemonInfo typeOfData="Speed" data={speed}></PokemonInfo>
                     </div>
                 )}
             </div>
---
+++ src/components/pokemonInfo.jsx
@@ -0,0 +1,8 @@
+export const PokemonInfo = ({typeOfData, data}) => {
+    return (
+        <div className="flex justify-between items-center py-0 px-4">
+            <span className="text-base font-normal">{typeOfData}</span>
+            <span className="text-base font-bold">{data}</span>
+        </div>
+    );
+}
