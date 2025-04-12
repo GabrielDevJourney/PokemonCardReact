d08cb129a057d4ff1174c174c6763dc0ba3d3d34
Gabriel <gabspereira178@gmail.com>
2025-04-12 11:42:05 +0100

    implemented skeleton for when fetching data for a better ui and ux

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import { useState} from 'react';
+import { useEffect, useState } from 'react';
 import { PokemonCard } from './components/pokemonCard';
 import { PokemonSelector } from './components/pokemonSelector';
 import { PokemonNotFoundAlert } from './components/pokemonNotFound';
 import { usePokemonData } from './hook/usePokemonData';
 import { DEFAULT_POKEMON } from './constants/pokemons';
+import { SkeletonPokemonCard } from './components/skeletonPokemonCard';
 
 export const App = () => {
     const [selectedPokemon, setSelectedPokemon] = useState(DEFAULT_POKEMON);
-
+    const [showSkeleton, setShowSkeleton] = useState(true);
     const { pokemonData, isLoading, error } = usePokemonData(selectedPokemon);
+
+    useEffect(() => {
+        setShowSkeleton(true);
+        const timer = setTimeout(() => {
+            setShowSkeleton(false);
+        }, 1500);
+        return () => clearTimeout(timer);
+    }, [selectedPokemon]);
 
     const handlePokemonSelect = pokemon => {
         setSelectedPokemon(pokemon);
@@ -24,19 +33,20 @@ export const App = () => {
         );
     };
 
-    return isLoading ? (
-        <div>Loading Pokemon Data...</div>
+    return isLoading || showSkeleton ? (
+        <div className="flex flex-col items-center mt-25">
+            {showDropdown()}
+            <SkeletonPokemonCard />
+        </div>
     ) : error ? (
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center mt-25">
             <PokemonNotFoundAlert name={selectedPokemon}></PokemonNotFoundAlert>
             {showDropdown()}
         </div>
     ) : pokemonData ? (
-        <div className="flex flex-col items-center">
-            <div className="h-30 w-100"></div>
+        <div className="flex flex-col items-center mt-25">
             {showDropdown()}
-            <PokemonCard pokemonData={pokemonData}
-            ></PokemonCard>
+            <PokemonCard pokemonData={pokemonData}></PokemonCard>
         </div>
     ) : (
         <div>something went wrong</div>
