25c9cc0acd8b009b282832c9808ab411d607b956
Gabriel <gabspereira178@gmail.com>
2025-04-16 17:24:03 +0100

    chore: remove old JavaScript/JSX files
    - Remove all .js/.jsx files that have been migrated to TypeScript
    - Clean up old JavaScript implementations
    - Complete TypeScript migration

--- src/components/pokemonInfo.jsx
+++
@@ -1,8 +0,0 @@
-export const PokemonInfo = ({ typeOfData, data }) => {
-    return (
-        <div className="flex items-center justify-between px-4 py-0">
-            <span className="text-base font-normal">{typeOfData}</span>
-            <span className="text-base font-bold">{data}</span>
-        </div>
-    );
-};
